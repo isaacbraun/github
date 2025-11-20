c0fe2609f56043fcd7c8b89af668ccb8a7dbcd32
Isaac Braun <isaacbraun2000@gmail.com>
2025-11-19 16:38:17 -0800

    fix(iterate): resolve async processing issues and improve reporting

--- rest.ts
+++ rest.ts
@@ -5,6 +5,9 @@ type IssuesList =
   Endpoints["GET /repos/{owner}/{repo}/issues"]["response"]["data"];
 type Issue = IssuesList[number];
 type Label = Exclude<Issue["labels"][number], string>;
+type PaginateParams =
+  Endpoints["GET /repos/{owner}/{repo}/issues"]["parameters"];
+type ActionResponse = "triggered" | "skipped" | "failed";
 
 interface RestParams {
   owner: string;
@@ -27,68 +30,90 @@ export default function Rest({ owner, repo }: RestParams) {
     return issues.filter((issue) => !issue.pull_request);
   }
 
-  async function syncEsriProductLabels(issue: Issue): Promise<void> {
+  async function syncEsriProductLabels(issue: Issue): Promise<ActionResponse> {
     const productLabels = getProductLabels(issue);
-    if (productLabels.length <= 0) {
-      return;
-    }
+    if (productLabels.length === 0) return "skipped";
 
     const firstLabel = productLabels[0];
     if (!firstLabel?.name) {
-      console.warn(
-        `#${issue.number}: Failed finding label name - ${issue.html_url}`,
-      );
-      return;
+      console.warn(`#${issue.number}: Missing label name - ${issue.html_url}`);
+      return "failed";
     }
 
-    await octokit.rest.actions.createWorkflowDispatch({
-      owner,
-      repo,
-      workflow_id: "issue-monday-sync.yml",
-      ref: "dev",
-      inputs: {
-        issue_number: issue.number.toString(),
-        event_type: "SyncActionChanges",
-        label_name: firstLabel.name,
-        label_color: firstLabel.color,
-        label_action: "added",
-      },
-    });
-    console.log(
-      `#${issue.number}: Syncing ${firstLabel.name} - ${issue.html_url}`,
-    );
+    try {
+      await octokit.rest.actions.createWorkflowDispatch({
+        owner,
+        repo,
+        workflow_id: "issue-monday-sync.yml",
+        ref: "dev",
+        inputs: {
+          issue_number: issue.number.toString(),
+          event_type: "SyncActionChanges",
+          label_name: firstLabel.name,
+          label_color: firstLabel.color,
+          label_action: "added",
+        },
+      });
+      console.log(
+        `#${issue.number}: Dispatched for ${firstLabel.name} - ${issue.html_url}`,
+      );
+      return "triggered";
+    } catch (error) {
+      console.error(
+        `#${issue.number}: Failed to dispatch - ${issue.html_url}`,
+        error,
+      );
+      return "failed";
+    }
   }
 
   interface IterateParams {
-    action: (issue: Issue) => Promise<void> | void;
+    action: (issue: Issue) => Promise<ActionResponse>;
+    state?: PaginateParams["state"];
+    milestone?: PaginateParams["milestone"];
     per_page?: number;
     onlyFirstPage?: boolean;
   }
   async function iterateAllIssues({
     action,
+    state = "open",
+    milestone,
     per_page = 30,
     onlyFirstPage = false,
   }: IterateParams): Promise<void> {
-    try {
-      for await (const page of octokit.paginate.iterator(
-        "GET /repos/{owner}/{repo}/issues",
-        {
-          owner,
-          repo,
-          per_page,
-        },
-      )) {
-        const issues = removePullRequests(page.data);
-        issues.forEach(async (issue) => await action(issue));
+    const counters = {
+      total: 0,
+      triggered: 0,
+      skipped: 0,
+      failed: 0,
+    };
 
-        if (onlyFirstPage) {
-          break;
-        }
-      }
-    } catch (error) {
-      console.error("Error fetching product issues:", error);
-      throw error;
+    let iteratorOptions: PaginateParams = { owner, repo, per_page, state };
+    if (milestone) {
+      iteratorOptions = { ...iteratorOptions, milestone };
     }
+
+    for await (const page of octokit.paginate.iterator(
+      "GET /repos/{owner}/{repo}/issues",
+      iteratorOptions,
+    )) {
+      const issues = removePullRequests(page.data);
+      counters.total += issues.length;
+
+      for (const issue of issues) {
+        const result = await action(issue);
+        counters[result] += 1;
+      }
+
+      if (onlyFirstPage) break;
+    }
+    console.log(
+      `\n --- Issues processed --- \n
+      Total: ${counters.total} \n
+      Triggered: ${counters.triggered} \n
+      Skipped: ${counters.skipped} \n
+      Failed: ${counters.failed} \n`,
+    );
   }
 
   return {
