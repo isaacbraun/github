148c47d88ced4143397f0613c180a0c4fca6e09b
Isaac Braun <isaacbraun2000@gmail.com>
2025-12-16 16:52:45 -0800

    chore: sync i18n label

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import Rest from "./rest";
+import { type WorkflowInputs } from "./types";
 
 const owner = "Esri";
 const repo = "calcite-design-system";
 const rest = Rest({ owner, repo });
 
-// await rest.iterateAllIssues({
-//   action: rest.syncEsriProductLabels,
-//   per_page: 100,
-//   sleepMs: 5000,
-// });
+const label = "i18n-l10n";
+const inputs: WorkflowInputs = {
+  label_name: label,
+  label_action: "added",
+}
 
-const issue = await rest.getIssue(12940);
-await rest.syncAssignees(issue);
+await rest.iterateAllIssues({
+  action: (issue) => rest.dispatchMondayWorkflow(issue, inputs),
+  label_filter: label,
+  per_page: 100,
+});
