42584e963218eea3cbad2c0f59265aa57aee47d1
Isaac Braun <isaacbraun2000@gmail.com>
2026-04-07 14:13:15 -0700

    feat: move run.ts to `run` folder

--- run/unrelatedMergedPrs.ts
+++ run/unrelatedMergedPrs.ts
@@ -1,16 +1,9 @@
 import GitRest from "./src/utils/git-rest";
-import { type WorkflowInputs } from "./src/types";
 import { unrelatedMergedPrs } from "./src/utils/issue-functions";
 
 const owner = "Esri";
 const repo = "calcite-design-system";
 const gitRest = GitRest({ owner, repo });
-
-const inputs: WorkflowInputs = {
-  // label_name: label,
-  // label_action: "added",
-  // assignee_updated: "true",
-};
 
 await gitRest.iterateAllIssues({
   action: (issue) => unrelatedMergedPrs(issue),
