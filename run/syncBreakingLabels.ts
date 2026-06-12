ad7c9e82414852ec2bbdf6e055d0eee4c713aee1
Isaac Braun <isaacbraun2000@gmail.com>
2026-06-12 15:38:47 -0700

    feat(run): add sync breaking change script (#8)

---
+++ run/syncBreakingLabels.ts
@@ -0,0 +1,31 @@
+import GitRest from "../src/utils/git-rest";
+import type { ActionResponse, Issue } from "../src/types";
+
+const owner = "Esri";
+const repo = "calcite-design-system";
+const gitRest = GitRest({ owner, repo });
+
+async function findAndSyncLabel(label: string): Promise<void> {
+  console.log(`Syncing issues with label: ${label}`);
+
+  await gitRest.iterateAllIssues({
+    action: (issue) => syncLabel(issue, label),
+    state: "open",
+    label_filter: label,
+    per_page: 100,
+  });
+}
+
+async function syncLabel(issue: Issue, label: string): Promise<ActionResponse> {
+  gitRest.dispatchMondayWorkflow(issue, {
+    label_action: "added",
+    label_name: label,
+  });
+
+  return "triggered";
+}
+
+await Promise.all([
+  findAndSyncLabel("breaking change"),
+  findAndSyncLabel("future breaking change"),
+]);
